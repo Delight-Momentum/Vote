39c5fa17d4e53d61692005044d63d6a6b2a23862
wecaners <wecaners@naver.com>
2024-04-01 17:35:58 +0900

    chore: test

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,11 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const nextJest = require('next/jest')
+import nextJest from 'next/jest'
 
-/** @type {import('jest').Config} */
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
-
-// Add any custom config to be passed to Jest
-const config = {
-  coverageProvider: 'v8',
-  testEnvironment: 'jest-environment-jsdom',
-  preset: 'ts-jest',
-  // Add more setup options before each test is run
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  // reporters: ['default', 'jest-junit'],
+const createJestConfig = nextJest()
+const customJestConfig = {
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom/extend-expect',
+    '@testing-library/react',
+  ],
 }
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(config)
+export default createJestConfig(customJestConfig)
