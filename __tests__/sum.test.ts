8fb1b7c7546ccf637f4a621f0e1d7c9036b635f2
lsc58461 <lsc58461@naver.com>
2024-04-01 17:04:54 +0900

    chore: CI 테스트 1

---
+++ __tests__/sum.test.ts
@@ -0,0 +1,13 @@
+import sum from '@/app/sum'
+
+test('adds 2 + 2 to equal 4', () => {
+  expect(sum(2, 2)).toBe(4)
+})
+
+test('adds 2 + 3 to equal 5', () => {
+  expect(sum(2, 3)).toBe(5)
+})
+
+test('adds 2 + 4 to equal 6', () => {
+  expect(sum(2, 4)).toBe(6)
+})
