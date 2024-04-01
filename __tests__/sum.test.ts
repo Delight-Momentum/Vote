872b0a1af8295f9a023b369aea63aef46ad4e8f9
namminimi <tbvhdaos@naver.com>
2024-04-01 17:09:20 +0900

    chore: ci test

--- __tests__/sum.test.ts
+++ __tests__/sum.test.ts
@@ -7,3 +7,7 @@ test('adds 2 + 3 to equal 5', () => {
 test('adds 2 + 4 to equal 6', () => {
   expect(sum(2, 4)).toBe(6)
 })
+
+test('adds 3 + 4 to equal 7', () => {
+  expect(sum(3, 4)).toBe(7)
+})
