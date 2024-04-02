1f8db1091c014b85b05c40c3eb0e3fda8234ff93
namminimi <tbvhdaos@naver.com>
2024-04-02 14:42:57 +0900

    feat: test2

--- __tests__/sum.test.ts
+++ __tests__/sum.test.ts
@@ -44,6 +44,6 @@ test('adds 2 + 4 to equal 6', () => {
   expect(sum(2, 4)).toBe(6)
 })
 
-test('adds 2 + 4 to equal 6', () => {
-  expect(sum(2, 4)).toBe(6)
+test('adds 3 + 4 to equal 7', () => {
+  expect(sum(3, 4)).toBe(7)
 })
