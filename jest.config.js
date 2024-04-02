1ce3b616ed4ed7957893140bbbb3311ea78db809
aowjarkwk <justgam12@gmail.com>
2024-04-02 18:02:03 +0900

    chore: jest ci 테스트36

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,14 @@
-/**
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
+const { Config } = require('jest')
 
-import type { Config } from 'jest'
-
-const config: Config = {
+const config = {
   coverageProvider: 'v8',
   moduleNameMapper: { '^@/(.*)$': '<rootDir>/$1' },
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
-
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
 }
 
-export default config
+module.exports = config
