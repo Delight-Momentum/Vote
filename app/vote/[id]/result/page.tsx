a73764572a326341592224d89dd3c4f327cafc8b
wecaners <wecaners@naver.com>
2024-05-10 17:15:50 +0900

    feat: 투표 결과 페이지 구현

---
+++ app/vote/[id]/result/page.tsx
@@ -0,0 +1,40 @@
+import ButtonRound from '@/components/button-round'
+import Header from '@/components/header'
+import ProgressBar from '@/components/progress-bar'
+import React from 'react'
+
+function ResultPage() {
+  return (
+    <>
+      <Header>투표결과</Header>
+      <div className="flex flex-col items-center">
+        <div className="mt-62pxr flex h-320pxr w-465pxr flex-col justify-center">
+          <div className="mb-48pxr flex flex-col gap-20pxr">
+            <h1 className="text-24pxr font-semibold leading-[36px] tracking-[0.12px]">
+              가장 최고의 아이돌은?
+            </h1>
+            <div className="flex flex-col gap-10pxr">
+              <ProgressBar
+                contentId={1}
+                voteItem="BTS"
+                choiceCount={300}
+                participantCounts={523}
+              />
+              <ProgressBar
+                contentId={2}
+                voteItem="빅뱅"
+                choiceCount={223}
+                participantCounts={523}
+              />
+            </div>
+          </div>
+          <ButtonRound variant="primary" size="lg">
+            결과 공유하기
+          </ButtonRound>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ResultPage
