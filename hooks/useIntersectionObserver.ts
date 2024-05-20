ad5997e3cafec720e42f8343716a4cedd28e5413
wecaners <wecaners@naver.com>
2024-05-20 11:27:30 +0900

    refactor: useIntersectionObserver hook 수정

--- hooks/useIntersectionObserver.ts
+++ hooks/useIntersectionObserver.ts
@@ -1,25 +1,28 @@
-import { useMemo, useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 
 function useIntersectionObserver() {
   const [isVisible, setIsVisible] = useState(true)
+  const observerRef = useRef<IntersectionObserver | null>(null)
 
-  const observer = useMemo(
-    () =>
-      new IntersectionObserver(
-        (entries) => {
-          setIsVisible(entries[0].isIntersecting)
-        },
-        { threshold: 0.8 },
-      ),
-    [],
-  )
+  useEffect(() => {
+    observerRef.current = new IntersectionObserver(
+      (entries) => {
+        setIsVisible(entries[0].isIntersecting)
+      },
+      { threshold: 0.8 },
+    )
+  }, [])
 
   const observe = (element: HTMLDivElement) => {
-    observer.observe(element)
+    if (observerRef.current && element) {
+      observerRef.current.observe(element)
+    }
   }
 
   const unobserve = (element: HTMLDivElement) => {
-    observer.unobserve(element)
+    if (observerRef.current && element) {
+      observerRef.current.unobserve(element)
+    }
   }
 
   return { observe, unobserve, isVisible, setIsVisible }
