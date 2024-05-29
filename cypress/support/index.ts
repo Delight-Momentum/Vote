a8a1079825cfca175f310c03bdd8177d11187789
wecaners <wecaners@naver.com>
2024-05-29 22:50:13 +0900

    refactor: cypress.json 추가

--- cypress/support/index.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-Cypress.on('uncaught:exception', (err: Error, runnable: Mocha.Runnable) => {
-  // returning false here prevents Cypress from failing the test
-  return false
-})
