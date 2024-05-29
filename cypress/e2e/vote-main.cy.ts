4d276afbd29369c8a43d935c6673238d75009858
lsc58461 <lsc58461@naver.com>
2024-05-29 23:48:55 +0900

    fix: 테스트 수정

--- cypress/e2e/vote-main.cy.ts
+++ cypress/e2e/vote-main.cy.ts
@@ -43,25 +43,36 @@ describe('메인페이지를 테스트 한다.', () => {
     cy.get('@voteCard').should('exist')
     cy.get('[data-cy=resultButton]').as('resultButton')
     cy.get('@resultButton').should('exist')
+
     cy.get('@resultButton').first().click()
     cy.url().should('match', /\/vote\/\d+\/result$/)
   })
 
   it('검색 기능을 테스트한다.', () => {
+    cy.wait(1500)
     cy.get('[data-cy=searchInput]').as('searchInput')
+
     cy.get('@searchInput').type('테스트 3')
+    cy.wait(500)
+
     cy.get('[data-cy=voteCardList]').as('voteCardList')
     cy.get('[data-cy=voteTitle]').as('voteTitle')
+
     cy.get('@voteCard').each(($card) => {
       cy.wrap($card).get('@voteTitle').should('include.text', '테스트 3')
     })
   })
 
   it('무한 스크롤을 테스트한다.', () => {
+    cy.wait(1500)
     cy.get('@voteCard').should('have.length', 8)
+
     cy.scrollTo('bottom')
+
     cy.get('@voteCard').should('have.length.gte', 8)
+
     cy.scrollTo('bottom')
+
     cy.get('@voteCard').should('have.length.gte', 16)
   })
 })
