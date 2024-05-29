3dd621ea364e7e49de766f8161b75a3c668ad6c2
wecaners <wecaners@naver.com>
2024-05-29 23:31:47 +0900

    refactor: cypress 환경변수 추가

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,5 +7,8 @@ export default defineConfig({
     defaultCommandTimeout: 10000,
     pageLoadTimeout: 60000,
     chromeWebSecurity: false,
+    env: {
+      NEXT_PUBLIC_KAKAO_API_KEY: process.env.NEXT_PUBLIC_KAKAO_API_KEY,
+    },
   },
 })
