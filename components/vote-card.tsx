7ddf5e2a2c2542125ed0585bc1e330f22c40aa61
lsc58461 <79037820+lsc58461@users.noreply.github.com>
2024-06-13 21:24:24 +0900

    feat: VoteCard 컴포넌트 제목, 내용에 툴팁 추가 (#103)

--- components/vote-card.tsx
+++ components/vote-card.tsx
@@ -1,5 +1,9 @@
+'use client'
+
 import Link from 'next/link'
+import { useState } from 'react'
 import CirCleButton from './circle-button'
+import Tooltip from './tooltip'
 
 interface Props {
   isClosed: boolean
@@ -10,6 +14,8 @@ interface Props {
   participateResultUrl: string
 }
 
+const MAX_VOTE_ITEMS = 3
+
 function VoteCard({
   isClosed,
   voteTitle,
@@ -18,15 +24,33 @@ function VoteCard({
   participateUrl,
   participateResultUrl,
 }: Props) {
-  const MAX_VOTE_ITEMS = 3
-  const sliceVoteItems = voteItems?.slice(0, MAX_VOTE_ITEMS)
+  const [tooltip, setTooltip] = useState({
+    voteTitle: false,
+    voteItems: Array(MAX_VOTE_ITEMS).fill(false),
+  })
+
+  const sliceVoteItems = voteItems.slice(0, MAX_VOTE_ITEMS)
+
+  const handleMouseEnter = (index?: number) => {
+    setTooltip((prev) => ({
+      voteTitle: index === undefined ? true : prev.voteTitle,
+      voteItems: prev.voteItems.map((_, i) => i === index),
+    }))
+  }
+
+  const handleMouseLeave = (index?: number) => {
+    setTooltip((prev) => ({
+      voteTitle: index === undefined ? false : prev.voteTitle,
+      voteItems: prev.voteItems.map(() => false),
+    }))
+  }
 
   return (
     <div
       data-cy="voteCard"
       className="relative h-376pxr w-320pxr transform rounded-2xl bg-white px-25pxr pb-25pxr pt-17pxr transition duration-300 hover:scale-110"
     >
-      <Link href={`${isClosed ? participateResultUrl : participateUrl}`}>
+      <Link href={isClosed ? participateResultUrl : participateUrl}>
         {isClosed && (
           <div className="absolute left-0pxr top-0pxr z-50 flex h-full w-full flex-col items-center justify-center gap-10pxr rounded-2xl bg-[rgba(11,11,11,0.60)]">
             <p className="text-center text-16pxr font-medium text-white">
@@ -36,31 +60,44 @@ function VoteCard({
             </p>
           </div>
         )}
-        <div className="w-282pxr pb-17pxr">
+        <div className="relative w-282pxr pb-17pxr">
           <h2
             data-cy="voteTitle"
             className="overflow-hidden text-ellipsis whitespace-nowrap text-22pxr font-medium"
+            onMouseEnter={() => handleMouseEnter()}
+            onMouseLeave={() => handleMouseLeave()}
+            onTouchStart={() => handleMouseEnter()}
+            onTouchEnd={() => handleMouseLeave()}
           >
             {voteTitle}
           </h2>
+          {tooltip.voteTitle && (
+            <Tooltip arrowPosition="top">{voteTitle}</Tooltip>
+          )}
         </div>
         <div className="relative flex h-239pxr flex-col gap-15pxr pb-25pxr pt-10pxr">
-          {sliceVoteItems?.map((item, index) => (
+          {sliceVoteItems.map((item, index) => (
             <div
-              className={`relative flex items-center justify-between overflow-hidden rounded-md bg-[#eBeBeB] py-17pxr pl-25pxr pr-13pxr ${isClosed ? 'bg-[#e7e7e8]' : ''}`}
+              className={`relative flex items-center justify-between rounded-md bg-[#eBeBeB] py-17pxr pl-25pxr pr-13pxr ${isClosed ? 'bg-[#e7e7e8]' : ''}`}
               key={`${item}-${index + 1}`}
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={() => handleMouseLeave(index)}
+              onTouchStart={() => handleMouseEnter(index)}
+              onTouchEnd={() => handleMouseLeave(index)}
             >
               <div
-                className={`absolute left-0pxr top-0pxr h-full w-1/2 rounded-r-md bg-primary200 ${isClosed ? 'bg-[#999999]' : ''}`}
+                className={`absolute left-0pxr top-0pxr h-full w-1/2 rounded-md bg-primary200 ${isClosed ? 'bg-[#999999]' : ''}`}
               />
               <p className="z-10 w-190pxr overflow-hidden text-ellipsis text-nowrap font-[#49454f] text-16pxr font-normal">
                 {item}
               </p>
               <p className="font-[#49454f] text-16pxr font-normal">??%</p>
+              {tooltip.voteItems[index] && (
+                <Tooltip arrowPosition="left">{item}</Tooltip>
+              )}
             </div>
           ))}
-          <div className="absolute bottom-25pxr h-58pxr w-270pxr bg-[linear-gradient(180deg,rgba(255,255,255,0.00)_0%,#FFF_100%)]" />
-          {voteItems?.length > MAX_VOTE_ITEMS && (
+          {voteItems.length > MAX_VOTE_ITEMS && (
             <div className="absolute bottom-6pxr left-0pxr z-10 flex w-full items-center justify-center bg-[linear-gradient(180deg,rgba(255,255,255,0.00)_0%,#FFF_100%)]">
               <p className="text-14pxr font-normal text-[#49454F]">
                 +{voteItems.length - MAX_VOTE_ITEMS}개의 선택지가 더 있어요
@@ -72,10 +109,9 @@ function VoteCard({
       <div className="flex h-55pxr items-center justify-between">
         <div className="flex h-40pxr items-end pt-5pxr">
           <p
-            className={`${isClosed && 'flex h-full w-full items-center justify-center !text-[#999999]'} text-14pxr font-normal text-[#49454F]`}
+            className={`${isClosed ? 'flex h-full w-full items-center justify-center !text-[#999999]' : 'text-14pxr font-normal text-[#49454F]'}`}
           >
-            {participantsCount}
-            {isClosed ? '명 참여 완료' : '명 참여중'}
+            {participantsCount}명 {isClosed ? '참여 완료' : '참여중'}
           </p>
         </div>
         {!isClosed && (
