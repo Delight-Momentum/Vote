ab291e4354ebd037facd30f5ae5da017b8e0e080
lsc58461 <lsc58461@naver.com>
2024-06-17 21:24:13 +0900

    refactor: 메인페이지 검색, 필터 변경시 깜빡임 수정

--- components/vote-card-list.tsx
+++ components/vote-card-list.tsx
@@ -1,17 +1,30 @@
 import { IVote } from 'types/voteListType'
+import { RefObject } from 'react'
+import Link from 'next/link'
 import VoteCard from './vote-card'
 
 interface Props {
+  observerRef: RefObject<HTMLDivElement>
   voteList: IVote[]
   isLoading?: boolean
 }
 
-function VoteCardList({ voteList, isLoading }: Props) {
+function VoteCardList({ observerRef, voteList, isLoading }: Props) {
   return (
     <div
       data-cy="voteCardList"
-      className="grid grid-cols-1 place-items-center gap-27pxr md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4"
+      className="grid min-h-[70vh] grid-cols-1 place-items-center gap-27pxr md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4"
     >
+      {voteList.length === 0 && !isLoading && (
+        <div className="col-span-4 text-center">
+          <h1 className="text-32pxr">투표가 없어요.</h1>
+          <Link className="mt-4pxr" href="/create-vote">
+            <button className="text-18pxr underline" type="button">
+              투표 등록하러가기
+            </button>
+          </Link>
+        </div>
+      )}
       {isLoading ? (
         <h1>로딩중입니다!</h1>
       ) : (
@@ -31,6 +44,7 @@ function VoteCardList({ voteList, isLoading }: Props) {
           },
         )
       )}
+      <div className="h-1pxr w-1pxr" ref={observerRef} />
     </div>
   )
 }
