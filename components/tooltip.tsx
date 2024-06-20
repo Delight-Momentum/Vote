16aeefdf1b9f00713e560034940cd8ef74005efc
wecaners <wecaners@naver.com>
2024-06-21 00:52:33 +0900

    feat: 기명투표 툴팁 표시

--- components/tooltip.tsx
+++ components/tooltip.tsx
@@ -1,19 +1,34 @@
-import { ReactNode } from 'react'
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { ReactNode, forwardRef } from 'react'
 
 interface ITooltipProps {
   arrowPosition: 'top' | 'left'
   children: ReactNode
+  className?: string
+  onClick?: (e: React.MouseEvent) => void
 }
 
-function Tooltip({ arrowPosition, children }: ITooltipProps) {
-  const arrowClassName = `absolute  ${arrowPosition === 'top' ? '-top-5pxr left-1/2 -translate-x-1/2' : '-left-5pxr top-1/2 -translate-y-1/2'} h-10pxr w-10pxr rotate-45 bg-primary100`
+const Tooltip = forwardRef<HTMLDivElement, ITooltipProps>(function Tooltip(
+  { arrowPosition, children, className, onClick },
+  ref,
+) {
+  const arrowClassName = `absolute ${
+    arrowPosition === 'top'
+      ? '-top-5pxr left-1/2 -translate-x-1/2'
+      : '-left-5pxr top-1/2 -translate-y-1/2'
+  } h-10pxr w-10pxr rotate-45 bg-primary100`
 
   return (
-    <div className="absolute left-[25%] z-[999] flex max-h-350pxr max-w-215pxr rounded-lg bg-primary100 p-8pxr shadow-lg">
+    <div
+      className={`absolute left-[25%] z-[999] flex max-h-350pxr max-w-215pxr rounded-lg bg-primary100 p-8pxr shadow-lg ${className}`}
+      onClick={onClick}
+      ref={ref}
+    >
       <div className={arrowClassName} />
       <p className="text-13pxr text-black">{children}</p>
     </div>
   )
-}
+})
 
 export default Tooltip
