3a7539a1762ec765c2d649092e22b09eba7b02b0
lsc58461 <79037820+lsc58461@users.noreply.github.com>
2024-06-20 21:55:44 +0900

    [#106] refactor: 메인페이지 검색, 필터 변경시 깜빡임 수정 (#110)
    * refactor: 메인페이지 검색, 필터 변경시 깜빡임 수정
    
    * fix: cypress e2e 통과 못하는 문제 수정

--- components/vote-card-list.tsx
+++ components/vote-card-list.tsx
@@ -1,17 +1,47 @@
+'use client'
+
 import { IVote } from 'types/voteListType'
+import { RefObject, useEffect, useState } from 'react'
+import Link from 'next/link'
 import VoteCard from './vote-card'
 
 interface Props {
+  observerRef: RefObject<HTMLDivElement>
   voteList: IVote[]
   isLoading?: boolean
 }
 
-function VoteCardList({ voteList, isLoading }: Props) {
+function VoteCardList({ observerRef, voteList, isLoading }: Props) {
+  const [showNoVotesMessage, setShowNoVotesMessage] = useState(false)
+
+  useEffect(() => {
+    if (voteList.length === 0 && !isLoading) {
+      const timer = setTimeout(() => {
+        setShowNoVotesMessage(true)
+      }, 500)
+
+      return () => clearTimeout(timer)
+    }
+
+    setShowNoVotesMessage(false)
+    return undefined
+  }, [voteList, isLoading])
+
   return (
     <div
       data-cy="voteCardList"
-      className="grid grid-cols-1 place-items-center gap-27pxr md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4"
+      className="grid min-h-[50vh] grid-cols-1 place-items-center gap-27pxr md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4"
     >
+      {showNoVotesMessage && (
+        <div className="col-span-4 text-center">
+          <h1 className="text-32pxr">투표가 없어요.</h1>
+          <Link className="mt-4pxr" href="/create-vote">
+            <button className="text-18pxr underline" type="button">
+              투표 등록하러가기
+            </button>
+          </Link>
+        </div>
+      )}
       {isLoading ? (
         <h1>로딩중입니다!</h1>
       ) : (
@@ -31,6 +61,7 @@ function VoteCardList({ voteList, isLoading }: Props) {
           },
         )
       )}
+      <div className="h-1pxr w-1pxr" ref={observerRef} />
     </div>
   )
 }
